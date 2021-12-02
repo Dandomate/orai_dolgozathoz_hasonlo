43cd184ddfd81e38ae842b4aa3daaf63cb118d16
Dandomate <dandomate@gmail.com>
2021-12-02 11:45:49 +0100

    vált

--- index.js
+++ index.js
@@ -38,7 +38,21 @@ connection.connect((err) => {
         const sorok = JSON.parse(JSON.stringify(result));
         //console.log(sorok);
         for (sor of sorok) {
-            console.log(`Érték:  ${sor.ertek1}`);
+            console.log(`Bevétel:  ${sor.ertek1}`);
+        }
+    
+    });
+    const myQuery3 = "SELECT SUM(ertek) AS ertek1 FROM `eladas` where ertek < 0;";
+    connection.query(myQuery3, (err, result, fields) => {
+        if (err) throw err;
+
+        //console.log(result);
+        //console.log(JSON.stringify(result));
+        console.log('-----------------------------');
+        const sorok = JSON.parse(JSON.stringify(result));
+        //console.log(sorok);
+        for (sor of sorok) {
+            console.log(`Kiadás:  ${sor.ertek1}`);
         }
     
     });
